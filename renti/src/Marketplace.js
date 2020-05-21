bda0f77a36e11f772d2049442ec6806dbcb48e42
Tom1k <tomascosta@ua.pt>
2020-05-21 10:38:39 +0100

    Added card data and now displaying correctly

--- renti/src/Marketplace.js
+++ renti/src/Marketplace.js
@@ -69,7 +69,43 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const cards = [
+  {
+      "id" : "1",
+      "name" : "GoPro Hero5",
+      "image" : "https://www.backscatter.com/ITEM_IMAGES/gp-chdhx-501_1.jpg",
+      "location" : "Aveiro, Portugal",
+      "price" : "20.00"
+  },
+  {
+      "id" : "2",
+      "name" : "Muletas",
+      "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQaQif8TkLykf9ABY2Bm4JK_TE_F_o_zDdXkucVpHkUA9N07TQ1&usqp=CAU",
+      "location" : "Porto, Portugal",
+      "price" : "2.00"
+  },
+  {
+      "id" : "3",
+      "name" : "Boss RC-505",
+      "image" : "https://static.fnac-static.com/multimedia/Images/PT/NR/78/a6/0a/697976/1540-6/tsp20160818141512/Bo-Loop-Station-RC-505.jpg",
+      "location" : "Aveiro, Portugal",
+      "price" : "52.50"
+  },
+  {
+      "id" : "4",
+      "name" : "Fato Homem - M",
+      "image" : "https://www.youlikeitstore.com/wp-content/uploads/2019/08/8718475994657_a_en_hd_1.jpg",
+      "location" : "Lisboa, Portugal",
+      "price" : "18.00"
+  },
+  {
+      "id" : "5",
+      "name" : "Fato Homem - XL",
+      "image" : "https://www.youlikeitstore.com/wp-content/uploads/2019/08/8718475994657_a_en_hd_1.jpg",
+      "location" : "Lisboa, Portugal",
+      "price" : "19.30"
+  },
+];
 
 export default function Marketplace() {
   const classes = useStyles();
@@ -138,31 +174,31 @@ export default function Marketplace() {
           {/* End hero unit */}
           <Grid container spacing={4}>
             {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
+              <Grid item key={card.id} xs={12} sm={6} md={4}>
                 <Card className={classes.card}>
                   <CardMedia
                     className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
+                    image={card.image}
                     title="Image title"
                   />
                   <CardContent style={{textAlign: 'center'}} className={classes.cardContent}>
                     <Typography gutterBottom variant="h5" component="h3">
-                      Fato Homem - XL
+                      {card.name}
                     </Typography>
                   </CardContent>
                   <CardContent style={{textAlign: 'center'}} className={classes.cardContent}>
                     <Typography gutterBottom>
-                      Águeda, Aveiro
+                      {card.location}
                     </Typography>
                   </CardContent>
                   <Row style={{textAlign: 'center'}}>
-                    <CardContent className={classes.cardContent}>
+                    <CardContent style={{flex:1, alignItems: 'center', justifyContent: 'center', alignContent: 'center'}}>
                       <Typography gutterBottom>
-                        20.00€ /day
+                        {card.price}€ /day
                       </Typography>
                     </CardContent>
-                    <CardActions style={{alignItems: 'center', justifyContent: 'center', alignContent: 'center'}}>
-                      <Button size="large" style={{color: colors.primary}}>
+                    <CardActions style={{flex:1 ,alignItems: 'center', justifyContent: 'center', alignContent: 'center'}}>
+                      <Button size="large" style={{color: 'white',backgroundColor:colors.primary}}>
                         <MdPhone/>
                       </Button>
                     </CardActions>
