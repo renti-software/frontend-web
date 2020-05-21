84f898a220c62a401a44f6d42228cb15c2bf3b56
Tom1k <tomascosta@ua.pt>
2020-05-21 12:23:23 +0100

    Working Fetch + SearchBar

--- renti/src/Marketplace.js
+++ renti/src/Marketplace.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import colors from './Colors';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
@@ -55,46 +55,69 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const hook_cards = [
-  {
-      "id" : "1",
-      "name" : "GoPro Hero5",
-      "image" : "https://www.backscatter.com/ITEM_IMAGES/gp-chdhx-501_1.jpg",
-      "location" : "Aveiro, Portugal",
-      "price" : "20.00"
-  },
-  {
-      "id" : "2",
-      "name" : "Muletas",
-      "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQaQif8TkLykf9ABY2Bm4JK_TE_F_o_zDdXkucVpHkUA9N07TQ1&usqp=CAU",
-      "location" : "Porto, Portugal",
-      "price" : "2.00"
-  },
-  {
-      "id" : "3",
-      "name" : "Boss RC-505",
-      "image" : "https://static.fnac-static.com/multimedia/Images/PT/NR/78/a6/0a/697976/1540-6/tsp20160818141512/Bo-Loop-Station-RC-505.jpg",
-      "location" : "Aveiro, Portugal",
-      "price" : "52.50"
-  },
-  {
-      "id" : "4",
-      "name" : "Fato Homem - M",
-      "image" : "https://www.youlikeitstore.com/wp-content/uploads/2019/08/8718475994657_a_en_hd_1.jpg",
-      "location" : "Lisboa, Portugal",
-      "price" : "18.00"
-  },
-  {
-      "id" : "5",
-      "name" : "Fato Homem - XL",
-      "image" : "https://www.youlikeitstore.com/wp-content/uploads/2019/08/8718475994657_a_en_hd_1.jpg",
-      "location" : "Lisboa, Portugal",
-      "price" : "19.30"
-  },
-];
+const API_URL = "google.com";
 
 export default function Marketplace() {
   const classes = useStyles();
+
+  const [cards, setCards] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  function makeProductRequest() {
+    fetch(`http://dummy.restapiexample.com/api/v1/employees`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setCards([
+            {
+                "id" : "1",
+                "name" : "GoPro Hero5",
+                "image" : "https://www.backscatter.com/ITEM_IMAGES/gp-chdhx-501_1.jpg",
+                "location" : "Aveiro, Portugal",
+                "price" : "20.00"
+            },
+            {
+                "id" : "2",
+                "name" : "Muletas",
+                "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQaQif8TkLykf9ABY2Bm4JK_TE_F_o_zDdXkucVpHkUA9N07TQ1&usqp=CAU",
+                "location" : "Porto, Portugal",
+                "price" : "2.00"
+            },
+            {
+                "id" : "3",
+                "name" : "Boss RC-505",
+                "image" : "https://static.fnac-static.com/multimedia/Images/PT/NR/78/a6/0a/697976/1540-6/tsp20160818141512/Bo-Loop-Station-RC-505.jpg",
+                "location" : "Aveiro, Portugal",
+                "price" : "52.50"
+            },
+            {
+                "id" : "4",
+                "name" : "Fato Homem - M",
+                "image" : "https://www.youlikeitstore.com/wp-content/uploads/2019/08/8718475994657_a_en_hd_1.jpg",
+                "location" : "Lisboa, Portugal",
+                "price" : "18.00"
+            },
+            {
+                "id" : "5",
+                "name" : "Fato Homem - XL",
+                "image" : "https://www.youlikeitstore.com/wp-content/uploads/2019/08/8718475994657_a_en_hd_1.jpg",
+                "location" : "Lisboa, Portugal",
+                "price" : "19.30"
+            },
+          ])
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          alert("Error fetching products!")
+        }
+      )
+  }
+
+  useEffect(() =>  {
+    makeProductRequest()
+  });
 
   return (
     <React.Fragment>
@@ -121,7 +144,7 @@ export default function Marketplace() {
                   aria-describedby="basic-addon2"
                 />
                 <InputGroup.Append>
-                  <Button variant="primary" class="btn btn-primary" style={{backgroundColor:colors.primary}}>
+                  <Button onClick={() => makeProductRequest()} variant="primary" class="btn btn-primary" style={{backgroundColor:colors.primary}}>
                     <MdSearch/>
                   </Button>
                 </InputGroup.Append>
@@ -156,7 +179,8 @@ export default function Marketplace() {
         <Container className={classes.cardGrid} maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={4}>
-            {hook_cards.map((card) => (
+            { cards.map((card) => {
+              return true ?
               <Grid item key={card.id} xs={12} sm={6} md={4}>
                 <Card className={classes.card}>
                   <CardMedia
@@ -188,7 +212,9 @@ export default function Marketplace() {
                   </Row>
                 </Card>
               </Grid>
-            ))}
+            :
+              <Grid />
+            })}
           </Grid>
         </Container>
       </main>
