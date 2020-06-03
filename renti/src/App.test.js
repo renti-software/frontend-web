53165ac53b9f270b656e2434c03ecca7466aa0c3
miguelmatos-ua <miguelcruzmatos@ua.pt>
2020-06-03 18:10:31 +0100

    add test

--- renti/src/App.test.js
+++ renti/src/App.test.js
@@ -1,36 +1,58 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {fireEvent, render} from '@testing-library/react';
+import {createMemoryHistory} from 'history'
+import '@testing-library/jest-dom/extend-expect'
 import App from './App';
 import Marketplace from './Marketplace';
-import { useHistory } from 'react-router-dom';
+import {Router} from 'react-router-dom'
 
 //Start using mocks as in this template: testing-library.com/docs/react-testing-library
 
 //Mock user Storage
-localStorage.setItem('userID',1)
+localStorage.setItem('userID', 1);
 
 test('Render renti marketplace', () => {
-  const { getByText } = render(<App />);
+  const {getByText} = render(<App/>);
   const linkElement = getByText(/Renti Marketplace/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 
 test('Render footer', () => {
-  const { getByText } = render(<App />);
+  const {getByText} = render(<App/>);
   const linkElement = getByText(/Renti 2020/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('Render Remember me', () => {
-  const { getByText } = render(<App />);
+  const {getByText} = render(<App/>);
   const linkElement = getByText(/Remember me/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 //other screens testing
 test('Render Search Bar', () => {
-  const { getByText } = render(<Marketplace />);
+  const {getByText} = render(<Marketplace/>);
   const linkElement = getByText(/Rent anything!/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('Login goes to Sign Up', () => {
+  const history = createMemoryHistory()
+  history.push('/login')
+  const {getByText} = render(
+    <Router history={history}>
+      <App/>
+    </Router>
+  );
+
+  const linkElement = getByText(/Don't have an account\? Sign Up/i);
+  expect(linkElement).toBeInTheDocument();
+
+  fireEvent.click(linkElement);
+
+  const newElement = getByText(/Already have an account\? Sign in/i);
+
+  expect(newElement).toBeInTheDocument();
+
 });
